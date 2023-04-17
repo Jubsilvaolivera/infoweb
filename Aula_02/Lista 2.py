89168507fea8734a1ee586b2f52fb9090f60cbed
Julia (Juh) Oliveira <125093910+Jubsilvaolivera@users.noreply.github.com>
2023-04-17 13:21:21 +0000

    Initial commit

--- Aula_02/Lista 2.py
+++ Aula_02/Lista 2.py
@@ -1,28 +1,29 @@
-#print("Digite seu primeiro nome:")
-#A = input()
+#1
+print("Digite seu primeiro nome:")
+A = input()
 
-#print("Bem-vindo ao Python," , A)
-
-
-
-#print("Digite seu nome completo:")
-#A = input().split()
-
-#print("Bem-vindo ao Python," , A [0])
+print("Bem-vindo ao Python," , A)
 
 
+#2
+print("Digite seu nome completo:")
+A = input().split()
 
-#print("Digite a nota do primeiro bimestre da disciplina:")
-#n1 = int(input())
-#print("Digite a nota do segundo bimestre da disciplina:")
-#n2 = int(input())
-
-#M = (n1 * 2 + n2 * 3) / 5
-
-#print("Média parcial =" , M)
+print("Bem-vindo ao Python," , A [0])
 
 
+#3
+print("Digite a nota do primeiro bimestre da disciplina:")
+n1 = int(input())
+print("Digite a nota do segundo bimestre da disciplina:")
+n2 = int(input())
 
+M = (n1 * 2 + n2 * 3) / 5
+
+print("Média parcial =" , M)
+
+
+#4
 print("Digite a base e a altura do retângulo:")
 
 B = int(input())
@@ -32,4 +33,6 @@ A = int(input())
 Perímetro = B**2 + A**2
 Diagonal = (B**2 + A**2)**0.5
 
-print("Àrea -", Àrea , ("Perímetro -", Perímetro) , ("Diagonal -" , Diagonal))
+print("Àrea -", f"{Àrea:.2f}")
+print("Perímetro -", f"{Perímetro:.2f}")
+print("Diagonal -", f"{Diagonal:.2f}")
